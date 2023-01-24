43e59b427a2b411d2e1671f0e561588bad25b077
seohee0112 <ghdtj7@naver.com>
2023-01-24 16:24:42 +0900

    style : 아이콘 불러오기 추가

--- src/asset/icon/index.ts
+++ src/asset/icon/index.ts
@@ -1,1 +1,5 @@
+export { ReactComponent as IcFloggingStart } from './IcFlogging.svg';
 export { ReactComponent as IcLogo } from './IcLogo.svg';
+export { ReactComponent as IcTemporaryPause } from './IcPause.svg';
+export { ReactComponent as IcRunning } from './IcRunning.svg';
+export { ReactComponent as IcStop } from './IcStop.svg';
