4f9aacd163290fdce08f90cb9acfc7fb08dc8759
NaveOWO <pianowell96@gmail.com>
2023-01-25 02:14:13 +0900

    [style] 아이콘 삽입

--- src/asset/icon/index.ts
+++ src/asset/icon/index.ts
@@ -1,1 +1,4 @@
+export { ReactComponent as IcAlbum } from './IcAlbum.svg';
+export { ReactComponent as IcBackBtn } from './IcBackBtn.svg';
+export { ReactComponent as IcCamera } from './IcCamera.svg';
 export { ReactComponent as IcLogo } from './IcLogo.svg';
