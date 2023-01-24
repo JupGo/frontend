e555b8fec605cff4de729f0400ef5b57992628bf
seohee0112 <ghdtj7@naver.com>
2023-01-24 16:27:47 +0900

    docs : 가독성 개행 추가

--- src/lib/hooks/useCouter.tsx
+++ src/lib/hooks/useCouter.tsx
@@ -3,17 +3,20 @@ import { useCallback, useRef, useState } from 'react';
 const useCouter = (initMinutes: number, ms: number) => {
   const [count, setCount] = useState(initMinutes);
   const intervalRef = useRef<NodeJS.Timeout | null>(null);
+
   const start = useCallback(() => {
     if (intervalRef.current) return;
     intervalRef.current = setInterval(() => {
       setCount((prev) => prev + 1);
     }, ms);
   }, []);
+
   const stop = useCallback(() => {
     if (!intervalRef.current) return;
     clearInterval(intervalRef.current);
     intervalRef.current = null;
   }, []);
+
   return { count, start, stop };
 };
 
