c886f6e81d50cd71f52005761be2e242eaa02885
NaveOWO <pianowell96@gmail.com>
2023-02-01 03:01:56 +0900

    [feat] 사진 업로드 & 인증 기능 구현

--- src/pages/Authentication.tsx
+++ src/pages/Authentication.tsx
@@ -1,33 +1,101 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 
-import { IcAlbum, IcCamera } from '../asset/icon';
-import mainDefaultImg from '../asset/image/mainDefaultImg.png';
+import { IcBackBtn, IcMainDefault } from '../asset/icon';
 import subDefaultImg1 from '../asset/image/subDefaultImg1.png';
 import subDefaultImg2 from '../asset/image/subDefaultImg2.png';
 import subDefaultImg3 from '../asset/image/subDefaultImg3.png';
 import subDefaultImg4 from '../asset/image/subDefaultImg4.png';
+
 const Authentication = () => {
+  const userFile = useRef<HTMLInputElement | null>(null);
+  const [isUploaded, setIsUploaded] = useState<boolean>(false);
+  const [images, setImages] = useState<any>();
+  const [prevImage, setPrevImage] = useState<any>();
+  const [isPopUpOpen, setIsPopUpOpen] = useState<boolean>(false);
+  const [clickedImage, setClickedImage] = useState<string>();
+
+  const imageDownload = useRef<HTMLAnchorElement | null>(null);
+
+  const getUserFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setIsUploaded(true);
+    e.target.files && setImages(e.target.files[0]);
+    showPrevImage(e);
+  };
+
+  const showPrevImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const reader = new FileReader();
+    if (e.target.files && e.target.files[0]) {
+      reader.readAsDataURL(e.target.files[0]);
+    }
+    reader.onloadend = () => {
+      const resultImage = reader.result;
+      resultImage && setPrevImage(resultImage);
+    };
+  };
+
+  const downloadImage = () => {
+    imageDownload.current && imageDownload.current.click();
+  };
+
+  const closeUpExampleImage = (e: React.MouseEvent<HTMLImageElement>) => {
+    setClickedImage(e.currentTarget.src);
+  };
+
+  useEffect(() => {
+    clickedImage && setIsPopUpOpen(true);
+  }, [clickedImage]);
+
+  const closePopUp = () => {
+    setIsPopUpOpen(false);
+  };
+
   return (
     <Root>
       <AuthenticationHeader>
+        <BackButton />
         <Title>줍Go 인증</Title>
+        {isUploaded && <CompleteButton>완료</CompleteButton>}
       </AuthenticationHeader>
-      <MainImage src={mainDefaultImg} />
-      <SubImagesWrapper>
-        <SubImage src={subDefaultImg1} />
-        <SubImage src={subDefaultImg2} />
-        <SubImage src={subDefaultImg3} />
-        <SubImage src={subDefaultImg4} />
-      </SubImagesWrapper>
-      <ButtonWrapper>
-        <AlbumButton>
-          <IcAlbum />
-        </AlbumButton>
-        <CameraButton>
-          <IcCamera />
-        </CameraButton>
+      <MainImageSection>
+        {isUploaded ? (
+          <a download="줍Go_사진인증" href={prevImage} ref={imageDownload}>
+            <MainImage src={prevImage} />
+          </a>
+        ) : (
+          <EmptyIcon />
+        )}
+      </MainImageSection>
+      {!isUploaded && (
+        <SubImagesWrapper>
+          <SubImage src={subDefaultImg1} onClick={closeUpExampleImage} />
+          <SubImage src={subDefaultImg2} onClick={closeUpExampleImage} />
+          <SubImage src={subDefaultImg3} onClick={closeUpExampleImage} />
+          <SubImage src={subDefaultImg4} onClick={closeUpExampleImage} />
+        </SubImagesWrapper>
+      )}
+      <ButtonWrapper isUploaded={isUploaded}>
+        <div>
+          {isUploaded ? (
+            <DownloadButton onClick={downloadImage}>저장하기</DownloadButton>
+          ) : (
+            <label htmlFor="userFile">
+              <AlbumButton>사진 올리기</AlbumButton>
+            </label>
+          )}
+        </div>
+
+        <FindButton type="file" accept="image" multiple id="userFile" ref={userFile} onChange={getUserFile} />
       </ButtonWrapper>
+      {isPopUpOpen && (
+        <PopUpBackGround>
+          <PopUpTitleWrapper>
+            <PopUpTitle>예시사진</PopUpTitle>
+            <PopUpCloseButton onClick={closePopUp}>X</PopUpCloseButton>
+          </PopUpTitleWrapper>
+          <ClosedImage src={clickedImage} />
+        </PopUpBackGround>
+      )}
     </Root>
   );
 };
@@ -36,76 +104,204 @@ const Root = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  padding-bottom: 1.6rem;
 `;
 
 const AuthenticationHeader = styled.div`
-  height: 93px;
+  height: 9.3rem;
   width: 100%;
 
   background-color: ${({ theme }) => theme.colors.green_dark};
-  padding-top: 59px;
+  padding-top: 5.9rem;
+
+  display: flex;
+  align-items: center;
+`;
+
+const BackButton = styled(IcBackBtn)`
+  margin-left: 1.6rem;
 `;
 
 const Title = styled.h1`
   font-style: normal;
   font-weight: 700;
-  font-size: 16px;
-  line-height: 22px;
+  font-size: 1.6rem;
+  line-height: 2.2rem;
 
   color: white;
 
   display: flex;
   justify-content: center;
+
+  margin-left: 12.4rem;
+`;
+
+const CompleteButton = styled.div`
+  color: white;
+
+  font-weight: 400;
+  font-size: 1.8rem;
+  line-height: 22.5rem;
+
+  margin-left: 10.2rem;
+`;
+
+const MainImageSection = styled.article`
+  height: 32.8rem;
+  width: 32.8rem;
+
+  margin-top: 6.9rem;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  border: 0.3rem solid ${({ theme }) => theme.colors.green_dark};
+  border-radius: 2rem;
+
+  background-color: rgba(255, 255, 199, 0.5);
+
+  overflow: hidden;
 `;
 
 const MainImage = styled.img`
-  height: 328px;
-  width: 328px;
+  height: 32.8rem;
+  width: 32.8rem;
+`;
 
-  margin-top: 69px;
+const EmptyIcon = styled(IcMainDefault)`
+  height: 10rem;
+  width: 10rem;
 `;
 
 const SubImagesWrapper = styled.div`
-  width: 328px;
+  width: 32.8rem;
 
   display: flex;
   justify-content: space-around;
 
-  margin-top: 29px;
+  margin-top: 2.9rem;
 `;
 
 const SubImage = styled.img`
-  height: 61px;
-  width: 61px;
+  height: 6.1rem;
+  width: 6.1rem;
 `;
 
-const ButtonWrapper = styled.div`
-  width: 328px;
+const ButtonWrapper = styled.div<{ isUploaded: boolean }>`
+  width: 32.8rem;
 
   display: flex;
-  justify-content: space-around;
+  justify-content: center;
 
-  margin-top: 45px;
-  padding: 0 24px;
+  margin-top: ${({ isUploaded }) => (isUploaded ? 13 : 45)}px;
+  padding: 0 2.4rem;
+
+  position: relative;
 `;
 
-const AlbumButton = styled.button`
-  height: 110px;
-  width: 110px;
+const AlbumButton = styled.div`
+  height: 7.4rem;
+  width: 34.3rem;
 
-  border-radius: 50%;
+  border-radius: 1rem;
 
   border: none;
   background-color: ${({ theme }) => theme.colors.green_main};
-`;
-const CameraButton = styled.button`
-  height: 110px;
-  width: 110px;
 
-  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  font-style: normal;
+  font-weight: 700;
+  font-size: 1.6rem;
+  line-height: 2.2rem;
+
+  color: white;
+`;
+
+const DownloadButton = styled.div`
+  height: 7.4rem;
+  width: 34.3rem;
+
+  border-radius: 1rem;
 
   border: none;
   background-color: ${({ theme }) => theme.colors.green_main};
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  font-style: normal;
+  font-weight: 700;
+  font-size: 1.6rem;
+  line-height: 2.2rem;
+
+  color: white;
+`;
+
+const FindButton = styled.input`
+  height: 11rem;
+  width: 11rem;
+
+  display: none;
+`;
+
+const PopUpBackGround = styled.section`
+  height: 72.6rem;
+  width: 100%;
+
+  padding: 3.2rem 2.3rem 0 2.3rem;
+
+  position: absolute;
+  top: 9.3rem;
+
+  background-color: rgba(0, 0, 0, 0.76); ;
+`;
+
+const PopUpTitleWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const PopUpTitle = styled.h2`
+  font-weight: 700;
+  font-size: 1.6rem;
+  line-height: 2.2rem;
+
+  color: white;
+
+  margin-left: 13.6rem;
+`;
+
+const PopUpCloseButton = styled.div`
+  height: 4rem;
+  width: 4rem;
+
+  margin-left: 8.9rem;
+
+  background-color: white;
+  color: ${({ theme }) => theme.colors.black_01};
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  font-weight: 700;
+  font-size: 2rem;
+  line-height: 2.7rem;
+
+  border-radius: 1rem;
+`;
+
+const ClosedImage = styled.img`
+  height: 32.8rem;
+  width: 32.8rem;
+
+  margin-top: 4rem;
 `;
 
 export default Authentication;
