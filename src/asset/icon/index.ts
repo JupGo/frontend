898949801293ba43cc6f60585b02b2bf9b28fe6a
NaveOWO <pianowell96@gmail.com>
2023-02-01 03:01:37 +0900

    [style ] 아이콘 export

--- src/asset/icon/index.ts
+++ src/asset/icon/index.ts
@@ -2,3 +2,5 @@ export { ReactComponent as IcAlbum } from './IcAlbum.svg';
 export { ReactComponent as IcBackBtn } from './IcBackBtn.svg';
 export { ReactComponent as IcCamera } from './IcCamera.svg';
 export { ReactComponent as IcLogo } from './IcLogo.svg';
+export { ReactComponent as IcMainDefault } from './IcMainDefault.svg';
+export { ReactComponent as IcSubDefault } from './IcSubDefault.svg';
