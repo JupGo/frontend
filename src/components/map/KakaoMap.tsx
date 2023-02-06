318ff0b0ee67e0fe737596c46d520f5a9fc66682
seohee0112 <ghdtj7@naver.com>
2023-02-06 14:52:51 +0900

    feat : 이미지 마커 표시 및 이동 동선 표시 및 거리 계산 구현

--- src/components/map/KakaoMap.tsx
+++ src/components/map/KakaoMap.tsx
@@ -1,10 +1,16 @@
 import { useEffect, useState } from 'react';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import styled from 'styled-components';
 
-import { KakaoMapInfo } from '../../types/map';
+import MapCurrent from '../../asset/image/MapCurrent.png';
+import { floggingInfoState, mapLocationInfoState } from '../../recoil/atom';
+import { KakaoMapLocation } from '../../types/map';
+import { getDistanceKm } from '../../utils/getDistanceKm';
 
 const KakaoMap = () => {
-  const [kakaoMapInfo, setKakaoMapInfo] = useState<KakaoMapInfo>();
+  const mapLocationInfo = useRecoilValue(mapLocationInfoState);
+  const setFloggingInfo = useSetRecoilState(floggingInfoState);
+  const [kakaoMapInfo, setKakaoMapInfo] = useState<KakaoMapLocation>();
   const { kakao } = window;
 
   useEffect(() => {
@@ -23,13 +29,29 @@ const KakaoMap = () => {
       };
 
       const map = new kakao.maps.Map(container, options);
-      const markerPosition = new kakao.maps.LatLng(kakaoMapInfo.latitude, kakaoMapInfo.longitude);
 
+      const markerImgSrc = MapCurrent;
+      const markerImgSize = new kakao.maps.Size(window.screen.width * 0.08, window.screen.width * 0.08);
+      const markerImg = new kakao.maps.MarkerImage(markerImgSrc, markerImgSize);
+      const markerPosition = new kakao.maps.LatLng(kakaoMapInfo.latitude, kakaoMapInfo.longitude);
       const marker = new kakao.maps.Marker({
         position: markerPosition,
+        image: markerImg,
+      });
+
+      const linePath = mapLocationInfo.map((location) => new kakao.maps.LatLng(location.latitude, location.longitude));
+      const kakaoPolyline = new kakao.maps.Polyline({
+        path: linePath,
+        strokeWeight: 5,
+        strokeColor: '#EF8D8A',
+        strokeOpacity: 0.7,
+        strokeStyle: 'solid',
       });
 
       marker.setMap(map);
+      kakaoPolyline.setMap(map);
+
+      setFloggingInfo((prev) => ({ ...prev, distance: getDistanceKm(mapLocationInfo) }));
     }
   }, [kakaoMapInfo]);
 
